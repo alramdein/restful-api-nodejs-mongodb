1a0f7e13d63513c9e5a79430d710581d7a2b0718
alramdein <ramdanialif26@gmail.com>
2020-10-22 17:05:56 +0700

    added udpateUser

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -45,13 +45,34 @@ export const getUserByUsername = (req, res) => {
                 }
                 res.json(user);
             }).catch(err => {
-                if(err.kind === "ObjectId") {
-                    res.status(404).send({
-                        message: err.message || `Username ${req.params.username} not found`
-                    })
-                }
                 res.status(500).send({
                     message: err.message || `Error retrieving user with username ${req.params.username}`
                 })
             })
+};
+
+export const updateUser = (req, res) => {
+    // Validation request
+    if(!req.body.username || !req.body.password) {
+        return res.status(400).send({
+            message: "Username or password can't be empty"
+        })
+    }
+
+    User.findOneAndUpdate({username: req.params.username}, {
+        username: req.body.username,
+        password: req.body.password,
+    }, {new: true})
+        .then(user => {
+            if(!user) {
+                res.status(404).send({
+                    message: err.message || `Username ${req.params.username} not found`
+                })
+            }
+            res.json({"message": `User with username ${req.params.username} successfully updated!.`});
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || `Error deleting user with username ${req.params.username}`
+            })
+        })
 };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,7 +3,8 @@ import * as user from '../controller/user.controller' ;
 const userRoutes = (app) => {
     app.post('/api/v1/users', user.addUser);
     app.get('/api/v1/users', user.getAllUser);  
-    app.get('/api/v1/users/:username', user.getUserByUsername);    
+    app.get('/api/v1/users/:username', user.getUserByUsername);
+    app.put('/api/v1/users/:username', user.updateUser);      
 }
 
 export default userRoutes;
